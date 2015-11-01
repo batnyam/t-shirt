8921a6bd940d0264c193945ec5a9032be66cd424
Batnyam Baasanjav <hbatnyam@gmail.com>
2015-11-02 01:21:34 +0800

    Add to Bag
    Add to Bag and fixed some errors

--- js/controller/ItemController.js
+++ js/controller/ItemController.js
@@ -2,4 +2,20 @@ app.controller('ItemController', ['$scope', 'shop', function($scope, shop){
 	shop.success(function(data){
 		$scope.shop = data;
 	});
+
+	$scope.selectedItems = [];
+	
+	function setCardItem(id){
+		currentProduct = $scope.shop.products[id];
+		item = "<li><img src='"+currentProduct.thumb+"' class='product-image'></span><span class='product-name'>"+currentProduct.title+"</span><span class='product-price'>"+currentProduct.price+"</span></li>";
+		card = $('.dropdown-menu').html();
+		card += item;
+		$('.dropdown-menu').html(card);
+		$('.dropdown-toggle').attr("aria-expanded", true);
+	}
+
+	$scope.addCard = function(id){
+		$scope.selectedItems.push(id);
+		setCardItem(id);
+	}
 }]);
